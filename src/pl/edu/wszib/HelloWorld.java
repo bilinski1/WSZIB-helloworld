b9e3a296c213f8b0769963b9da0ecd9857c40c70
bilinski <abc@example.com>
2020-12-29 01:24:31 +0100

    Initial Commit: Hello Mars

--- src/pl/edu/wszib/HelloWorld.java
+++ src/pl/edu/wszib/HelloWorld.java
@@ -2,6 +2,7 @@ package pl.edu.wszib;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("-Hello World!" + "-Hello Mars!");
     }
+
 }
